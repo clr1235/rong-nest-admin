4261b6842f5f74f3c44edaf57660dbe35518a1d6
clr1235 <15035291867@163.com>
2025-08-21 11:20:40 +0800

    feat: 初始化server服务端项目并和前端项目进行跨域接口打通测试

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Axios from 'axios'
+import { onMounted, ref, type Ref } from 'vue'
+
+let str: Ref<string> = ref('')
+
+const msg = async () => {
+	return await Axios.get('http://localhost:3000/api/').then((res) => {
+		console.log(res.data)
+		return res.data
+	})
+}
+
+onMounted(async () => {
+	str.value = await msg()
+})
 </script>
 
 <template>
 	<div>
-		<a href="https://vite.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		{{ str }}
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()]
+	plugins: [vue()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 })
