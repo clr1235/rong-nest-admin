16c9d0451fd6d7260558493f4224b91fd6478a4c
clr1235 <15035291867@163.com>
2025-08-21 10:53:57 +0800

    chore: 文件调整、初始化admin项目

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,5 +13,8 @@ export default defineConfig([
 	},
 	tseslint.configs.recommended,
 	pluginVue.configs['flat/essential'],
-	{ files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } }
+	{
+		files: ['**/*.vue'],
+		languageOptions: { parserOptions: { parser: tseslint.parser } }
+	}
 ])
