a7efa90bc8d0e78dc670091a085f6b4d9269d62f
clr1235 <15035291867@163.com>
2025-08-23 12:01:23 +0800

    feat: 尝试tailwindcss

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '@/views/Home/index.vue'
 import AboutView from '@/views/About/index.vue'
+import Login from '@/views/login/index.vue'
 
 const routes = [
 	{ path: '/', component: HomeView },
-	{ path: '/about', component: AboutView }
+	{ path: '/about', component: AboutView },
+	{ path: '/login', component: Login }
 ]
 
 const router = createRouter({
--- admin/src/views/login/index.vue
+++ admin/src/views/login/index.vue
@@ -1,7 +1,25 @@
 <template>
-	<div>登录</div>
+	<div class="app-container">
+		<div class="flex h-full">
+			<div class="flex flex-col flex-auto">
+				<div class="login-background absolute left-0 top-0 size-full"></div>
+				<div class="flex-none h-[120px]">logo</div>
+				<div class="flex-1 flex flex-col items-center justify-center">
+					<SloganIcon class="animate-float h-64 w-2/5" />
+				</div>
+			</div>
+			<div class="flex-none w-120">右</div>
+		</div>
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SloganIcon from './icons/slogan.vue'
+</script>
 
-<style lang="less" scoped></style>
+<style lang="scss" scoped>
+.login-background {
+	background: linear-gradient(154deg, #07070915 30%, hsl(var(--primary) / 30%) 48%, #07070915 64%);
+	filter: blur(100px);
+}
+</style>
