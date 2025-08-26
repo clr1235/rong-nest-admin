a2e27e3f0560876238a0055d275b34d0bed0078f
clr1235 <15035291867@163.com>
2025-08-26 17:49:14 +0800

    feat: 修改eslint规则

--- eslint.config.ts
+++ eslint.config.ts
@@ -33,7 +33,8 @@ export default defineConfig([
 			}
 		},
 		rules: {
-			'@typescript-eslint/no-explicit-any': 'off'
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off'
 		}
 	}
 ])
